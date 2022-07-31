ec2ecd1d883eca528de66099ac977845761383a6
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-07-31 12:55:15 +0300

    adds main js file to import countries and webtech

---
+++ Ex/lvl2/main.js
@@ -0,0 +1,5 @@
+import { webTechs, showWebTechs } from './webTechs';
+import { countries, showCountries } from './countries';
+
+console.log(showCountries(countries));
+console.log(showWebTechs(webTechs));
