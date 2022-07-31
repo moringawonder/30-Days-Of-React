cb968106450c01ac9e804ec0559965309d17fee0
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-07-31 14:23:47 +0300

    adds lv2 ex soln

---
+++ Ex/lvl2/Lvl2_soln/exercise.js
@@ -0,0 +1,21 @@
+// Empty object called dog
+const dog = {
+    neme:'shelby',
+    legs:4,
+    color:'ombre',
+    age:6,
+}
+dog.barkProp = function (){
+    let bark = true
+    if(bark==bark){
+        console.log('woof woof')
+    }
+    
+}
+dog.breed = 'Wonder'
+dog.getDogInfo = function () {
+    return `${dog}`
+}
+console.log(dog)
+
+
